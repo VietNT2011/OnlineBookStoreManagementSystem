90bc7cc75bf1c251e613c7049d02daf7a5e4e599
tieenluowng03 <luongthicamtien2003@gmail.com>
2024-04-25 22:24:45 +0700

    OBMS-36: Add to cart

--- AgileBookStore/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AgileBookStore.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
-    }
-}
